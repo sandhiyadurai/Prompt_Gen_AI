883a45a584a8bc2181d13ea9a3f2f5de34246895
Sandhiya durai <duraisandhiya623@gmail.com>
2025-07-15 16:38:02 +0530

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -15,7 +15,7 @@ st.set_page_config(
 # === CUSTOM CSS ===
 st.markdown("""
     <style>
-    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;600&display=swap');
+    @import url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOYYfY2VeRo5987cYYdjiykP6yXgazkvRl0XvKDeI1dXlKxRcWsMpG7QKO9NMbiAx7Y_A&usqp=CAU');
     html, body, [class*="css"]  {
         font-family: 'Poppins', sans-serif;
         background: linear-gradient(135deg, #fdf6f0, #f0f8ff);
